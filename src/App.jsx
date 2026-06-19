face50b38ad9984fc25d43b7addebd4f8c937215
Rubayed Ahmed Foysal <rubayedahmedfoysal@gmail.com>
2026-06-19 23:38:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="bg-amber-600 text-white font-bold text-3xl py-3">
+      Nexora: An AI-Powered Collaborative Learning Platform
+    </h1>
+  );
+}
+
+export default App;
